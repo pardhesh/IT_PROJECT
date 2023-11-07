830da3c88b5886309f9a43e71a8ee357134e5a13
rohitmagham <148303357+rohitmagham@users.noreply.github.com>
2023-11-07 22:11:36 +0530

    Update register.php
    I updated some changes in the registration page

--- routes/register.php
+++ routes/register.php
@@ -1,12 +1,12 @@
 <html>
     <head>
-        <title>Online voting system - Registratrion</title>
+        <title>Registratrion Page</title>
         <link rel="stylesheet" href="../css/stylesheet.css">
         <style>
             fieldset{
              
-                background-color:lightblue;
-                border: 1px solid #000;
+                background-color:lemonchiffon;
+                border: 2px solid #000;
                 float:center;
     
             }
@@ -15,7 +15,7 @@
     <body>
         <center>
             <div id="headerSection">
-            <h1>WELCOME TO ONLINE VOTING WEBSITE</h1>  
+            <h1>ONLINE VOTING WEBSITE</h1>  
             </div>
             <hr>
             <fieldset>
@@ -25,20 +25,20 @@
                     <input type="text" name="name" placeholder="Name" required>&nbsp
                     <input type="number" name="mob" placeholder="Mobile" required><br><br>
                     <input type="password" name="pass" placeholder="Password" required>&nbsp
-                    <input type="password" name="cpass" placeholder="Confirm Password" required><br><br>
-                    <input style="width: 31%" type="text" name="add" placeholder="Address" required><br><br>
-                    <div id="upload" style="width: 30%">
-                        Upload image: <input type="file" id="profile" name="image" required>
+                    <input type="password" name="cpass" placeholder="Confirm Password" required><br><br>&nbsp
+                    <input style="width: 31%" type="text" name="add" placeholder="Address" required><br><br>&nbsp
+                    <div id="upload" style="width: 50%">
+                        Upload your image: <input type="file" id="profile" name="image" required>
                     </div><br>
-                    <div id="upload" style="width: 30%">
-                        Select your role:
+                    <div id="upload" style="width: 50%">
+                        Select your role here:
                         <select name="role">
-                            <option value="1">Voter</option>
-                            <option value="2">Group</option>
+                            <option value="1">VOTER</option>
+                            <option value="2">CANDIDATE</option>
                         </select><br>                   
                     </div><br>
                     <button id="loginbtn" type="submit" name="registerbtn">Register</button><br><br>
-                        Already an user? <a href="../">Click here to Login</a>
+                        Already registered? <a href="../">Click to Login</a>
                 </form>
                 </fieldset>
             </center>
