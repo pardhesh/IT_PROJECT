f55c705096e205612545d5e94f29172805915f57
pardhesh <148302780+pardhesh@users.noreply.github.com>
2023-11-08 09:13:44 +0530

    Add files via upload

---
+++ online-voting-system.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` text NOT NULL,
+  `mobile` bigint(10) NOT NULL,
+  `password` int(11) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `photo` varchar(255) NOT NULL,
+  `status` int(11) NOT NULL,
+  `votes` int(11) NOT NULL,
+  `role` int(11) NOT NULL
+) 
+
